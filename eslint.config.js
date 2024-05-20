440a037004d5626b85b59d8136b1a359e08f5ba8
Nicholai Nissen <nicholainissen@gmail.com>
2024-05-20 14:46:54 +0200

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,33 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu(
   {
-    unocss: true,
     formatters: true,
+    unocss: true,
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.json', './apps/*/tsconfig.json'],
+      },
+    },
+  },
+  {
+    rules: {
+      'ts/no-redeclare': 'off',
+      'ts/consistent-type-definitions': [
+        'error',
+        'type',
+      ],
+      'antfu/top-level-function': 'off',
+      'vue/valid-v-slot': ['error', {
+        allowModifiers: true,
+      }],
+      'style/space-before-function-paren': ['error', {
+        anonymous: 'always',
+        named: 'always',
+        asyncArrow: 'always',
+      }],
+      'functional/no-classes': 'off',
+    },
   },
 )
