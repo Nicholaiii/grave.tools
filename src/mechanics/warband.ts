c244e28f7567adfea93b8e14ec045c18a93f7a7d
Nicholai Nissen <nicholainissen@gmail.com>
2024-08-16 22:17:35 +0200

    feat: add apprentice to the warband schema

--- src/mechanics/warband.ts
+++ src/mechanics/warband.ts
@@ -1,9 +1,11 @@
 import * as S from '@effect/schema/Schema'
 import { Wizard } from './wizard'
 import { Soldier } from './soldier/model'
+import { Apprentice } from './apprentice'
 
 export const Warband = S.Struct({
   wizard: Wizard,
+  apprentice: Apprentice,
   soldiers: S.optional(S.Array(Soldier).pipe(
     S.maxItems(8),
   ), { default: () => [] }),
