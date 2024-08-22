cd41219482ec4c10898f67e67945273f7034d135
Nicholai Nissen <nicholainissen@gmail.com>
2024-08-22 19:57:22 +0200

    feat: add item model and functions

--- src/mechanics/item/function.ts
+++ src/mechanics/item/function.ts
@@ -1,17 +1,33 @@
-import { Array, Data, Effect, Struct, pipe } from 'effect'
+import { Array, Data, Effect, Match, Predicate, Struct, pipe } from 'effect'
+import type { Unit } from '../unit'
 import type { Inventory, Item } from './model'
 
 export class InventoryFullError extends Data.TaggedError('InventoryFull') {}
+export class IncompatibleItemError extends Data.TaggedError('IncompatibleItem') {}
 
 type addItemToInventory = (inv: Inventory) => (item: Item) => Effect.Effect<Inventory, InventoryFullError>
-export const addItemToInventory: addItemToInventory = inv => item =>
-  inv.items.length >= inv.limit
-    ? Effect.fail(new InventoryFullError())
-    : Effect.succeed(
-      pipe(
-        inv,
-        Struct.evolve({
-          items: items => Array.append(items, item),
-        }),
-      ),
-    )
+export const addItemToInventory: addItemToInventory = inv => item => inv.items.length >= inv.limit
+  ? Effect.fail(new InventoryFullError())
+  : Effect.succeed(
+    pipe(
+      inv,
+      Struct.evolve({
+        items: items => Array.append(items, item),
+      }),
+    ),
+  )
+
+const isLegalSpellcasterItem = Predicate.some<Item>([
+
+])
+
+export const addItemToUnit = (unit: Unit) => (item: Item): Effect.Effect<Unit, InventoryFullError | IncompatibleItemError> =>
+  Match.value(unit).pipe(
+    Match.tag('Apprentice', () => isLegalSpellcasterItem(item) ? Effect.succeed(unit) : Effect.fail(new IncompatibleItemError())),
+    Match.tag('Wizard', () => isLegalSpellcasterItem(item) ? Effect.succeed(unit) : Effect.fail(new IncompatibleItemError())),
+    Match.tag('Soldier', () => Effect.succeed(unit)),
+    Match.exhaustive,
+  ).pipe(
+    Effect.flatMap(unit => Effect.all({ unit: Effect.succeed(unit), inventory: addItemToInventory(unit.inventory)(item) })),
+    Effect.map(({ unit, inventory }) => Struct.evolve(unit, { inventory: () => inventory })),
+  )
--- src/mechanics/item/model.ts
+++ src/mechanics/item/model.ts
@@ -1,19 +1,53 @@
 import * as S from '@effect/schema/Schema'
 import { Stat } from '../stats'
+import { Spell } from '../spell'
 
+/* Bonus */
 export const Bonus = S.Tuple(Stat, S.Number)
+export type Bonus = typeof Bonus.Type
 
-export const Item = S.Struct({
+/* Base Item Kinds */
+export const BaseItemKind = S.Literal(
+  'Dagger',
+  'Hand Weapon',
+  'Two-Handed Weapon',
+  'Staff',
+  'Bow',
+  'Crossbow',
+  'Shield',
+  'Light Armour',
+  'Heavy Armour',
+)
+export type BaseItemKind = typeof BaseItemKind.Type
+
+/* BaseItem */
+export const BaseItem = S.Struct({
+  kind: BaseItemKind,
   name: S.NonEmpty,
   bonuses: S.Array(Bonus),
   notes: S.optional(S.String),
 })
+export type BaseItem = typeof BaseItem.Type
 
+/* Spell Item *Kind */
+export const SpellItemKind = S.Literal('Scroll', 'Grimoire')
+export type SpellItemKind = typeof SpellItemKind.Type
+
+/* Spell Item */
+export const SpellItem = S.Struct({
+  kind: SpellItemKind,
+  name: S.NonEmpty,
+  spell: Spell,
+  notes: S.optional(S.String),
+})
+
+/* Item */
+export const Item = S.Union(BaseItem, SpellItem)
 export type Item = typeof Item.Type
 
+/* Inventory */
 export const Inventory = S.Struct({
   items: S.Array(Item),
   limit: S.NonNegative,
 })
-
 export type Inventory = typeof Inventory.Type
