94a8c37264f9fc9390502763d406507e073a5e23
Nicholai Nissen <nicholainissen@gmail.com>
2024-08-22 19:56:54 +0200

    feat(soldier): add inventory to model

--- src/mechanics/soldier/model.ts
+++ src/mechanics/soldier/model.ts
@@ -1,9 +1,13 @@
 import * as S from '@effect/schema/Schema'
 import { Stats } from '../stats'
+import { Inventory } from '../item/model'
 
 export const Soldier = S.TaggedStruct('Soldier', {
   name: S.NonEmpty,
   stats: Stats,
+
+  inventory: S.optional(Inventory, { default: () => ({ items: [], limit: 1 }) }),
+
   cost: S.NonNegative,
   specialist: S.optional(S.Boolean, { default: () => false }),
 })
